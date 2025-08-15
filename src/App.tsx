aabbe761d4e6d670a721f91da01bb176b9d7952a
archig97 <archismitaghosh@yahoo.com>
2025-08-15 08:32:59 +0530

    vite version change

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import  { useMemo, useState } from "react";
 import BotConfigForm from "./components/BotConfigForm";
 import LogsPanel from "./components/LogPanel";
 import { MODEL_OPTIONS} from "./components/BotConfigForm";
--- src/components/LogPanel.tsx
+++ src/components/LogPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 interface LogEntry {
   timestamp: string;
